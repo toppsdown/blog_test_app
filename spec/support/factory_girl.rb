27ee55ccb86a9145f8dbf9e5cb05a82902ee0138
Ethan <ethan@swipely.com>
2015-12-31 08:13:37 -0500

    add factory girl linting

--- spec/support/factory_girl.rb
+++ spec/support/factory_girl.rb
@@ -5,5 +5,13 @@ RSpec.configure do |config|
     # required for some reason due to
     # https://github.com/rails/spring/issues/88
     FactoryGirl.reload
+
+    if Rails.env.test?
+      begin DatabaseCleaner.start
+        FactoryGirl.lint
+      ensure
+        DatabaseCleaner.clean
+      end
+    end
   end
 end
